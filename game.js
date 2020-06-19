76e2cd95e76962973a7e2ffb2b0f4686f608733b
Rahul NAMBIAR <rahul.nambiar@alstomgroup.com>
2020-06-19 17:03:06 +0530

    new game

---
+++ game.js
@@ -0,0 +1,188 @@
+const canvas = document.getElementById("canvas");
+const contextv = canvas.getContext("2d");
+const canvasSize = 600;
+
+canvas.width = canvasSize;
+canvas.height = canvasSize;
+
+const snakeBox = 20;
+const totalMoves = canvasSize/snakeBox;
+
+const apple = new Image();
+apple.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/757px-Apple_logo_black.svg.png"
+//apple.width =20;
+//apple.height=20;
+
+//Declare audio here
+
+//
+
+
+//define snake
+
+let snake =[];
+snake[0] = {
+    x : 9*snakeBox,
+    y : 10*snakeBox
+};
+
+//create food
+
+let food ={};
+getFood();
+//score
+
+let score = 0;
+
+//snake directions
+let dir = ""
+//-------------------//
+document.addEventListener("keydown", handleKeyInit);
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            handleKey("ml");
+        } else {
+            handleKey("mr");
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            handleKey("mu");
+        } else { 
+            handleKey("md");
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+//-------------------//
+function handleKeyInit(){
+    handleKey("kb");
+}
+
+function handleKey(ValueMK = ""){
+    let key
+    if(ValueMK == "kb"){
+        key = event.keyCode;    
+    }else{
+        key = ValueMK == "ml"? 37 : ValueMK == "mr"? 39 : ValueMK == "mu"? 38 : 40 ;
+    }
+    if (key ==37 && dir!="Right"){
+        dir = "Left";
+    }else if(key ==38 && dir!="Down"){
+        dir = "Up";       
+    }else if(key ==39 && dir!="Left"){
+        dir = "Right";       
+    }else if(key ==40  && dir!="Up"){
+        dir = "Down";       
+    }
+
+}
+
+function getFood(){
+ food ={
+     x: Math.floor(Math.random()*(totalMoves-2-3)+ 3)*snakeBox,
+     y: Math.floor(Math.random()*(totalMoves-2-3)+ 3)*snakeBox
+ }
+}
+
+function collit(head, sar){
+    for(var i =0; i<sar.length; ++i){
+        if(sar[i].x == head.x && sar[i].y == head.y){
+            return true;
+        }
+    }
+     return false;
+}
+
+
+
+function finalrender(){
+    contextv.fillStyle = "#dcdcdc";
+    contextv.fillRect(0,0,canvasSize,canvasSize);
+
+    for(let i=0; i<snake.length;i++){
+        contextv.fillStyle = i==0?"#4caf50":"#ffffff";
+        contextv.fillRect(snake[i].x,snake[i].y,snakeBox,snakeBox); 
+
+        contextv.strokeStyle = "E91E63";
+        contextv.strokeRect(snake[i].x,snake[i].y,snakeBox,snakeBox);
+    }
+
+    contextv.drawImage(apple,food.x,food.y,snakeBox,snakeBox);
+    
+    let snakeX=snake[0].x;
+    let snakeY=snake[0].y;
+
+    if(dir=="Left") snakeX-=snakeBox;
+    if(dir=="Right") snakeX+=snakeBox;
+    if(dir=="Up") snakeY-=snakeBox;
+    if(dir=="Down") snakeY+=snakeBox;
+
+    //if snake eats food
+    if(snakeX == food.x && snakeY==food.y){
+        score++;
+        getFood();
+    }else{
+        snake.pop();
+    }
+
+    let newHead = {
+        x : snakeX,
+        y :  snakeY   
+    }
+
+    if(snakeX<0 || snakeX>=canvasSize || snakeY<0 || snakeY >= canvasSize || collit(newHead,snake)){
+        gameOver();
+        return;
+    }
+
+    snake.unshift(newHead);
+
+    contextv.fillStyle = "black";
+    contextv.font = "40px Arial";
+    contextv.fillText(score,10,40);
+
+}
+
+finalrender();
+
+var gm = setInterval(finalrender,120);  
+
+function gameOver(){
+    clearInterval(gm);
+    contextv.fillStyle ="black";
+    contextv.font = "60px";
+    contextv.fillText("Game Over | Score : "+score ,canvasSize/2-200,canvasSize/2);
+    document.getElementById("relo").style.visibility="visible";
+    
+}
